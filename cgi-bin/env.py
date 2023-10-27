a95ba8672aae1425acb47166daa6dcbf30f67456
dagmarkramer <kramerdagmar@gmail.com>
2023-10-27 15:08:50 +0200

    environment fixed

--- cgi-bin/env.py
+++ cgi-bin/env.py
@@ -1,5 +1,21 @@
 #!/usr/local/bin/python3
-import sys, os, time
+import os
 print("HTTP/1.1 200 OK")
-print("content-type: text/html\n\n")
-print(os.environ)
+
+# Set the content type to HTML
+print("Content-type: text/html\n\n")
+
+# Get environment variables
+env_variables = os.environ
+
+# Generate an HTML response
+print("<html>")
+print("<head><title>Environment Variables</title></head>")
+print("<body>")
+print("<h1>Environment Variables:</h1>")
+print("<ul>")
+for key, value in env_variables.items():
+    print(f"<li><b>{key}:</b> {value}</li>")
+print("</ul>")
+print("</body>")
+print("</html>")
